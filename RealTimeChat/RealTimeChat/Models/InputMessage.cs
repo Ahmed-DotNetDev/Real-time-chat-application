3860e8cc14716063e8feb3979ae6bba18d91f012
Ahmed.net <Ahmed.net@gmail.com>
2023-07-14 16:37:55 +0300

    ADD model and controller

---
+++ RealTimeChat/RealTimeChat/Models/InputMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace RealTimeChat.Models
+{
+    public class InputMessage
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
